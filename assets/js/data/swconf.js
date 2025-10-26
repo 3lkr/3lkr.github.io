39de62eedf51a67f40eb88915e7c936e2dc509c6
3lacker <ossama@danger-zone.rev.sfr.net>
2025-10-26 18:56:12 +0100

    First blog

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761500284',resources: [
+    cacheName: 'chirpy-1761501135',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
